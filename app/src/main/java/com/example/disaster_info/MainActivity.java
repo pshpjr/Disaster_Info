143c07827f30fe4a5e4d89613d1fbe6c69c71bdb
pshpjr <pshpjr@naver.com>
2021-07-14 16:17:08 +0900

    EmergencyActivity로 가는 버튼 제거하고 이 액티비티는 푸시 알림을 했을때만 실행함

--- app/src/main/java/com/example/disaster_info/MainActivity.java
+++ app/src/main/java/com/example/disaster_info/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -18,11 +19,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         startService(new Intent(MainActivity.this, CheckEmergencyService.class));
 
+        Intent i = getIntent();
+        if(i.getBooleanExtra("Emergency",false)){
+            startActivity(new Intent(MainActivity.this,EmergencyActivity.class));
+        }
+
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,EmergencyActivity.class));
             }
         });
+
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Log.d("main","getIntent");
+        Intent i = getIntent();
+        if(i.getExtras() == null) Log.d("main","empty intent");
+        if(i.getBooleanExtra("Emergency",false)){
+            startActivity(new Intent(MainActivity.this,EmergencyActivity.class));
+        }
     }
 }
