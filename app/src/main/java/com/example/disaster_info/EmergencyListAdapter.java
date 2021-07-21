dbf4a87bea0de8b3c22163f0b9cedf1136db0562
pshpjr <pshpjr@naver.com>
2021-07-21 21:43:08 +0900

    리스트 추가

---
+++ app/src/main/java/com/example/disaster_info/EmergencyListAdapter.java
@@ -0,0 +1,57 @@
+package com.example.disaster_info;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.StrictMode;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import java.util.ArrayList;
+
+public class EmergencyListAdapter extends BaseAdapter {
+    private TextView itemTextView;
+
+    private ArrayList<String> listViewItemList = new ArrayList<>();
+
+    @Override
+    public int getCount() {
+        return listViewItemList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return listViewItemList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
+        // "listview_item" Layout을 inflate하여 convertView 참조 획득.
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.emergency_list_item, parent, false);
+        }
+        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+        itemTextView = (TextView) convertView.findViewById(R.id.textView);
+        // 아이템 내 각 위젯에 데이터 반영
+        itemTextView.setText(listViewItemList.get(pos));
+
+        return convertView;
+
+    }
+    public void addItem(String s) {
+        listViewItemList.add(s);
+    }
+
+}
